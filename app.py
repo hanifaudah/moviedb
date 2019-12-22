af03b9f4d5f0438413ea365979ebf7d70344c3aa
hanifaudah <hanifarkanaudah@gmail.com>
2019-12-22 21:24:41 +0700

    added details page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,request,render_template
 from fetch import movie, movie_collection
+import json
+import requests
 app = Flask(__name__)
 
 @app.route('/',methods = ["GET","POST"])
@@ -13,3 +15,15 @@ def landing():
         movie_dict.results = []
         movie_dict.fetch(id)
         return render_template("landing.html",movie=movie_dict.results)
+
+@app.route('/details/<id>',methods=["GET"])
+def details(id):
+    url = "http://api.themoviedb.org/3/movie/" + id + "?api_key=95368f360e3dc457d2f213e11967e205"
+    data = json.loads(requests.get(url).text)
+    data = movie(data["id"],data["title"],data["poster_path"],data["vote_average"],data["release_date"],data["overview"])
+    return render_template("details.html",movie=data)
+
+# if __name__=="__main__":
+#     url = "http://api.themoviedb.org/3/movie/557?api_key=95368f360e3dc457d2f213e11967e205"
+#     data = json.loads(requests.get(url).text)
+#     print(data["id"])
--- fetch.py
+++ fetch.py
@@ -7,12 +7,14 @@ movie_dict = {}
 
 # Main movie class
 class movie:
-    def __init__(self,title="None",poster_url="None",score="None",date="None",overview="None"):
+    def __init__(self,id="",title="None",poster_url="None",score="None",date="None",overview="None"):
+        self.id = id
         self.title = title
         self.poster = "http://image.tmdb.org/t/p/w200" + str(poster_url)
         self.score = score
         self.date = date
         self.overview = overview
+
 
 class movie_collection:
     def __init__(self,results=[]):
@@ -20,12 +22,12 @@ class movie_collection:
     def fetch(self,url):
         results = json.loads(requests.get(url).text)["results"]
         for i in results:
-            self.results.append(movie(i["title"],i["poster_path"],i["vote_average"],i["release_date"],i["overview"]))
+            self.results.append(movie(i["id"],i["title"],i["poster_path"],i["vote_average"],i["release_date"],i["overview"]))
 
-if __name__=="__main__":
-    mov = movie_collection()
-    mov.fetch("http://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=spiderman")
-    for i in mov.results:
-        print(i.title,i.poster,i.score,i.date,i.overview)
-        print()
+# if __name__=="__main__":
+#     mov = movie_collection()
+#     mov.fetch("http://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=spiderman")
+#     for i in mov.results:
+#         print(i.id,i.title,i.poster,i.score,i.date,i.overview)
+#         print()
         
